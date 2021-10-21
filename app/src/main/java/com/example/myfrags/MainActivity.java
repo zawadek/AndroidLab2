4344feb22198a729e4566df70314bd197424a2ee
zawadek <zawadkad89@wp.pl>
2021-10-21 15:55:31 +0200

    Lab3 pierwsza zmiana

--- app/src/main/java/com/example/myfrags/MainActivity.java
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.myfrags;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     private FragmentManager fragmentManager;
 
