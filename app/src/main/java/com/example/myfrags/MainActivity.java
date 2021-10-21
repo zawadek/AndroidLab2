06107f4a311026d7e4da44628c1e230cdcfb0196
zawadek <zawadkad89@wp.pl>
2021-10-21 16:35:03 +0200

    Lab3 zmieniony layout, dodany interfejs, zmiany w mainActivity

--- app/src/main/java/com/example/myfrags/MainActivity.java
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myfrags;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -10,16 +11,22 @@ import android.os.Bundle;
 
 public class MainActivity extends FragmentActivity {
 
-    private FragmentManager fragmentManager;
+    //Wersja z lab 2 poniżej
+    /*private FragmentManager fragmentManager;
 
-    private Fragment fragment1, fragment2, fragment3, fragment4;
+    private Fragment fragment1, fragment2, fragment3, fragment4;*/
+
+    //Wersja z lab 3 poniżej
+    private int frames[];
+    private boolean hiden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragment1 = new Fragment1();
+        //zakomentowana wersja z lab2
+        /*fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
         fragment4 = new Fragment4();
@@ -32,7 +39,23 @@ public class MainActivity extends FragmentActivity {
         transaction.add(R.id.frame3, fragment3);
         transaction.add(R.id.frame4, fragment4);
         transaction.addToBackStack(null);
-        transaction.commit();
+        transaction.commit();*/
 
+        //wersja lab 3
+        if(savedInstanceState == null){
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hiden = false;
+        }else{
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hiden = savedInstanceState.getBoolean("HIDEN");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDEN", hiden);
     }
 }
